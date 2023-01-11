ea629ac4eba3de44c07d42fdc578d31d27e73f2d
3782291211 <Laklouk98@hotmail.com>
2023-01-11 11:44:06 +0000

    minor update

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
-//import { useSearchParams } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import * as api from '../api';
 import ArticlePreviewCard from './ArticlePreviewCard';
@@ -10,15 +9,6 @@ const [articles, setArticles] = useState([]);
 const [isLoading, setIsLoading] = useState(false);
 const { topic } = useParams();
 const [sortBy, setSortBy] = useState('created_at');
-
-/*const [searchParams, setSearchParams] = useSearchParams({});
-const sortBy = searchParams.get('sort_by');
-
-const setSortBy = option => {
-  const newParams = new URLSearchParams(searchParams);
-  newParams.set('sort_by', option);
-  setSearchParams(newParams);
-}*/
 
 useEffect(() => {
   setIsLoading(true);
@@ -65,18 +55,3 @@ return (
 
 export default Articles;
 
-/*
-const [sortBy, setSortBy] = useState('created_at');
-   <p>Sort by:</p>
-     
-     <select onChange={e => setSortBy(e.target.value)} value={sortBy}>
-       <option value="comment_count">Comment count</option>
-       <option value="title">Title</option>
-       <option value="author">Author</option>
-       <option value="votes">Votes</option>
-       <option value="created_at">Date</option>
-       <option value="topic">Topic</option>
-     </select>
-
-
-*/ 
