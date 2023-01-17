8feeadfe9fa913f8f3823376e0e16c40c39135b5
3782291211 <Laklouk98@hotmail.com>
2023-01-17 01:42:50 +0000

    Adding login button for individual users

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import * as api from '../api';
 
-const Users = () => {
+const Users = ({setLoggedInUser, setAvatarUrl, loggedInUser}) => {
 const [users, setUsers] = useState([]);
 const [isLoading, setIsLoading] = useState(false);
 const [error, setError] = useState(false);
+const [showLoggedInMsg, setShowLoggedInMsg] = useState(false);
 
 useEffect(() => {
     setIsLoading(true);
@@ -17,7 +18,15 @@ useEffect(() => {
     setError(err.message)
   });
 }, []);
-console.log(users);
+
+const handleClick = (username, avatar_url) => () => {
+  setLoggedInUser(username);
+  setAvatarUrl(avatar_url);
+  setShowLoggedInMsg(true);
+  window.scrollTo(0, 0);
+  setTimeout(() => setShowLoggedInMsg(false), 6000);
+};
+
 if (error) {
   return <p className="error">{error}</p>
 } else if (!error && isLoading) {
@@ -26,12 +35,16 @@ if (error) {
  return (
   <main>
   <h2>Users</h2>
+  {showLoggedInMsg && <p className="login__confirmation">You are now logged in as {loggedInUser}</p>}
     <ul className="users__list">
     {users.map(({username, name, avatar_url}) => {
-      return <li className="users__user">
+      return <li className="users__li" key={avatar_url} >
+        <div className="users__user">
         <p>{name}</p>
         <p style={{'fontStyle': 'italic'}}>"{username}"</p>
         <img src={avatar_url} alt={`${name}'s profile picture`} />
+        </div>
+        <button className="users__change" onClick={handleClick(username, avatar_url)}>Sign in as user</button>
       </li>
     })}
     </ul>
