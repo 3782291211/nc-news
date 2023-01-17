3522688b7f099c57bafb91518f48533e7292d855
3782291211 <Laklouk98@hotmail.com>
2023-01-17 01:38:45 +0000

    Limiting number of votes to 1 per article/comment

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,14 +1,42 @@
 import { useState } from "react";
 import * as api from '../api';
+import updateVotes from '../updateVotes';
 
-const Buttons = ({votes, showError, updateVotes, loggedInUser, author, id, setComments, setShowDeletedMsg, setShowDeleteError}) => {
+const Buttons = ({votes, showError, setShowError, loggedInUser, author, id, setItem, setShowDeletedMsg, setShowDeleteError, itemType}) => {
   
 const [notLoggedInError, setNotLoggedInError] = useState(false);
 const [disableDelete, setDisableDelete] = useState(false);
 const [isLoading, setIsLoading] = useState(false);
 
-const handleClick = () => 
-    !loggedInUser ? () => setNotLoggedInError(true) : updateVotes;
+const [previousButton, setPreviousButton] = useState('');
+const [voteLimitError, setVoteLimitError] = useState(false);
+
+const handleClick = e => {
+  console.log('hiya');
+if (e.target.id !== previousButton) {
+  setVoteLimitError(false);
+  setPreviousButton(e.target.id);
+  updateVotes(setShowError, setItem, id, e.target.id, itemType);
+} else {
+  setVoteLimitError(true);
+}
+
+}
+
+//setPreviousButton(e.target.id);
+//setVoteCount(prev => {});
+ // if (voteCount <= 1) {
+ //  updateVotes(setShowError, setItem, id, e.target.id, itemType);
+ // }
+//}
+
+/*
+
+if (!loggedInUser) {
+    setNotLoggedInError(true);
+  } else if
+*/
+//!loggedInUser ? () =>  : updateVotes;
 
 const handleDelete = () => {
   if (loggedInUser === author) {
@@ -18,7 +46,7 @@ const handleDelete = () => {
     api.deleteComment(id)
     .then(() => {
       setIsLoading(false);
-      setComments(prev => prev.filter(comment => 
+      setItem(prev => prev.filter(comment => 
       comment.comment_id !== id));
       setShowDeletedMsg(true);
       setTimeout(() => setShowDeletedMsg(false), 5000);
@@ -35,14 +63,16 @@ const handleDelete = () => {
   return (
     <div className="votes__buttons">
       <span className={`votes__count ${votes >= 0 ? '--positive' : '--negative'}`}>Votes: {votes}</span>
-      <button id="upvote" onClick={handleClick()}>Vote up 👍</button>
-      <button id="downvote" onClick={handleClick()}>Vote down 👎</button>
+      <button id="upvote" onClick={handleClick}>Vote up 👍</button>
+      <button id="downvote" onClick={handleClick}>Vote down 👎</button>
 
       {loggedInUser && loggedInUser === author && <button id={id} disabled={disableDelete} onClick={handleDelete} className={isLoading ? '--red' : ''}>{isLoading ? 'Deleting...' : 'Delete'}</button>}
    
-     {notLoggedInError && !loggedInUser && <p style={{'color': 'red', 'margin': '0 10px'}}>You must log in to vote.</p>}
+     {notLoggedInError && !loggedInUser && <p className="votes__error">You must log in to vote.</p>}
+
+     {voteLimitError && <p className="votes__error">{`You can only vote once per ${itemType}`}</p>}
      
-     {showError && <p style={{'color': 'red', 'margin': '0 10px'}}>Unable to update votes.</p>}
+     {showError && <p className="votes__error">Unable to update votes.</p>}
    </div>
   );
 };
