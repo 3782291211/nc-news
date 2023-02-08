0beb8320a19c8491656cf1e5393c7a37dbd635a0
3782291211 <Laklouk98@hotmail.com>
2023-02-08 00:08:20 +0000

    Modifying appearance and loading behaviour of nav section

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -18,6 +18,31 @@ return(
     <Link to="topics"><li className="nav__link">Topics</li></Link>
     <Link to="users"><li className="nav__link">Users</li></Link>
     </ul>
+    {!apiError && 
+    <DropdownButton disabled={isLoading} variant="primary" id="dropdown__main" title={isLoading ? <div style={{display: "inline-flex", alignItems: 'center'}}><Spinner style={{margin: '0 10px 0 0'}} as="span" size="sm" role="status" animation="grow" /><p style={{margin: 0}}>Loading topics...</p></div> : "📰 Articles by topic"}>
+      {topics.filter(({number_of_articles}) => number_of_articles > 0).map(({slug, topic_id}) => {
+        return <Dropdown.Item 
+        key={topic_id}
+        onClick={(e => navigate(`articles?topic=${e.target.innerHTML}`))}
+        >{slug}</Dropdown.Item>
+      } )}
+    </DropdownButton> }
+  </nav>
+)
+};
+
+export default Nav;
+
+/*
+
+return(
+  <nav>
+    <ul id="nav">
+    <Link to="/"><li className="nav__link">Home</li></Link>
+    <Link to="articles"><li className="nav__link">All articles</li></Link>
+    <Link to="topics"><li className="nav__link">Topics</li></Link>
+    <Link to="users"><li className="nav__link">Users</li></Link>
+    </ul>
     {isLoading && <div><Spinner animation="grow" /><p className="nav__loading">Fetching topics</p></div>}
     {!isLoading && !apiError && 
     <DropdownButton id="dropdown__main" title="📰 Articles by topic">
@@ -32,4 +57,4 @@ return(
 )
 };
 
-export default Nav;
+*/
