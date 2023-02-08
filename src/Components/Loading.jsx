8fba8f7d1a92088e3794db5ca85529472901726b
3782291211 <Laklouk98@hotmail.com>
2023-02-08 00:20:58 +0000

    Updating loading component

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -11,11 +11,16 @@ const messages = [
     'Almost done...',
     'Nearly there...',
     'Server is taking longer than expected to respond. Hang in there!',
-    'Just a few more moments...']
+    'Just a few more moments...',
+    'It\'s not broken, it\'s just really slow.',
+    'Loading'
+  ];
     
 useEffect(() => {
-if (index < 6) {
+if (index < 4) {
   setTimeout(() => setIndex(index + 1), 4000);
+} else if (index < 8) {
+  setTimeout(() => setIndex(index + 1), 6000);
 };
 }, [index]);
 
