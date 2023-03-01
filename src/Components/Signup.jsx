176bc1bf8a3564b7fee4de2aeafc4cc6afece1e1
3782291211 <Laklouk98@hotmail.com>
2023-03-01 23:50:43 +0000

    Refactoring states

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import * as api from '../api';
-import Spinner from 'react-bootstrap/Spinner';
+import AccountForm from "./AccountForm";
 
 const Signup = () => {
-const [screenName, setScreenName] = useState('');
-const [username, setUsername] = useState('');
-const [password, setPassword] = useState('');
-const [passwordConfirm, setPasswordConfirm] = useState('');
-const [avatarUrl, setAvatarUrl] = useState('');
+const [formData, setFormData] = useState({screenName: '', username: '', password: '', passwordConfirm: '', avatarUrl: ''});
+const {screenName, username, password, passwordConfirm, avatarUrl} = formData;
+
 const [isLoading, setIsLoading] = useState(false);
 const [successMsg, setSuccessMsg] = useState('');
 const [errorMsg, setErrorMsg] = useState('');
@@ -25,11 +23,7 @@ const handleSubmit = e => {
     api.signup(screenName, username, password, avatarUrl)
     .then(() => {
         setIsLoading(false);
-        setScreenName('');
-        setUsername('');
-        setPassword('');
-        setPasswordConfirm('');
-        setAvatarUrl('');
+        setFormData({screenName: '', username: '', password: '', passwordConfirm: '', avatarUrl: ''});
         setSuccessMsg("You've successfully created a new account. You can now sign in with your username and password.");
     })
     .catch(err => {
@@ -48,26 +42,7 @@ return (<main>
     {successMsg && !errorMsg && <p className="account__confirmation">{successMsg}</p>}
     {errorMsg && <p className="error">{errorMsg}</p>}
 
-    <form id="account__form" onSubmit={handleSubmit}>
-        <label className="account__form-label" htmlFor="account__new-name">Screen name</label>
-        <input id="account__new-name" type="text" onChange={e => setScreenName(e.target.value)} value={screenName} placeholder="Enter screen name"/>
-  
-        <label className="account__form-label" htmlFor="account__new-username">Username</label>
-        <input id="account__new-username" type="text" onChange={e => setUsername(e.target.value)} value={username} placeholder="Enter username"/>
-
-        <label className="account__form-label" htmlFor="account__new-password">Password *</label>
-        <input id="account__new-pasword" type="password" onChange={e => setPassword(e.target.value)} value={password} placeholder="Enter password"/>
-
-        <label className="account__form-label" htmlFor="account__new-password-conf">Confirm password</label>
-        <input id="account__new-password-conf" type="password" onChange={e => setPasswordConfirm(e.target.value)} value={passwordConfirm} placeholder="Confirm password"/>
-
-        <label className="account__form-label" htmlFor="account__new-avatar">Avatar URL <span style={{fontSize: 16}}>(optional)</span></label>
-        <input id="account__new-avatar" type="text" onChange={e => setAvatarUrl(e.target.value)} value={avatarUrl} placeholder="Enter avatar URL address"/>
-
-        <p style={{margin: '5px 0 0 5px', textAlign: 'left', fontSize: 16}}>*At least 10 characters long, including at least 1 number and 1 capital letter</p>
-
-        <button disabled={isLoading} style={isLoading ? {backgroundColor: 'blue', color: 'yellow'} : {}} id="account__submit">{!isLoading ? 'Complete sign up' : <><Spinner style={{width: 25, height: 25, fontSize: 15, margin: '0 7px -3px 0'}} animation="border" />Loading...</>}</button>
-    </form>
+    <AccountForm type="signup" formData={formData} setFormData={setFormData} handleSubmit={handleSubmit} isLoading={isLoading}/>
 </main>);
 };
 
