e6459d32a93c2b3aeadae536e61735ff9e5160ec
3782291211 <Laklouk98@hotmail.com>
2023-03-04 06:07:02 +0000

    Adding user context

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { TopicsProvider } from './Contexts/Topics';
+import { UserProvider } from './Contexts/CurrentUser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserProvider>
     <TopicsProvider>
       <App />
     </TopicsProvider>
+    </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
