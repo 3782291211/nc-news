6f2f810a892594e8f68211ea4f851c5dbfa87267
3782291211 <Laklouk98@hotmail.com>
2023-03-04 06:16:00 +0000

    Using uuid to generate key props

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react";
 import * as api from '../api';
 import Loading from "./Loading";
 import anonymous from '../anonymous.webp';
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
 
-const Users = ({setLoggedInUser, setAvatarUrl, loggedInUser}) => {
+const Users = () => {
 const [users, setUsers] = useState([]);
 const [isLoading, setIsLoading] = useState(false);
 const [error, setError] = useState(false);
-const [showLoggedInMsg, setShowLoggedInMsg] = useState(false);
-const navigate = useNavigate();
 
 useEffect(() => {
-    setIsLoading(true);
+  setIsLoading(true);
   api.fetchUsers().then(({users}) => {
     setUsers(users);
     setIsLoading(false);
@@ -23,9 +22,8 @@ useEffect(() => {
   });
 }, []);
 
-
 const handleError = ({ currentTarget }) => {
-  currentTarget.onerror = null; // prevents looping
+  currentTarget.onerror = null;
   currentTarget.src = anonymous;
 };
 
@@ -37,16 +35,17 @@ if (error) {
  return (
   <main>
   <h2>Users</h2>
-  {showLoggedInMsg && <p className="login__confirmation">You are now logged in as {loggedInUser}</p>}
     <ul className="users__list">
     {users.map(({username, name, avatar_url}) => {
-      return <li className="users__li" key={avatar_url} >
+      return <li className="users__li" key={uuidv4()} >
         <div className="users__user">
-        <p>{name}</p>
-        <p style={{'fontStyle': 'italic'}}>"{username}"</p>
+        <div>
+        <p className="users__details">{username}</p>
+        <p className="users__details">{name}</p>
+        <Link to={`/articles?author=${username}`} className="users__link">View articles</Link>
+        </div>
         <img src={avatar_url || ''} alt={name} onError={handleError}/>
         </div>
-        <button className="users__change" onClick={() => navigate(`/articles?author=${username}`)}>User's articles</button>
       </li>
     })}
     </ul>
