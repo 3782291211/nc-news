2b5e17ae975a47060eef8cf892a895e167ca3f95
3782291211 <Laklouk98@hotmail.com>
2023-03-05 22:25:54 +0000

    Adding topic form validation

--- src/Components/Topics.jsx
+++ src/Components/Topics.jsx
@@ -48,8 +48,12 @@ const handleSubmit = e => {
   if (!newTopic || !newTopicBody) {
     setError('You must provide a topic name and description.');
     setTimeout(() => setError(false), 6000);
-  }
-  if (newTopic && newTopicBody) {
+  };
+
+  if (!/^(\s*\w\s*)+$/.test(newTopic)) {
+    setError('Topic name can only contain letters, numbers and whitespaces.');
+    setTimeout(() => setError(false), 6000);
+  } else if (newTopic && newTopicBody) {
     setNewTopicLoading(true);
     api.postNewTopic(newTopic, newTopicBody)
     .then((topic) => {
@@ -57,15 +61,15 @@ const handleSubmit = e => {
       setSuccessMsg(true);
       setTimeout(() => setSuccessMsg(false), 6000);
       setTopics(prev => [topic, ...prev]);
-  })
-  .catch(err => {
-    setNewTopicLoading(false);
-    setError(err.message);
-    setTimeout(() => setError(false), 6000);
-  })
-  }
-  setNewTopic('');
-  setNewTopicBody('');
+      setNewTopic('');
+      setNewTopicBody('');
+    })
+    .catch(err => {
+      setNewTopicLoading(false);
+      setError(err.response?.data?.msg);
+      setTimeout(() => setError(false), 6000);
+    })
+  };
 };
 
 const handleDelete = topic => {
@@ -79,7 +83,7 @@ const handleDelete = topic => {
       setTimeout(() => setShowDeletedMsg(false), 6000);
      })
      .catch(err => {
-       setError(err.message);
+       setError(err.response?.data?.msg);
        setTimeout(() => setError(false), 6000);
      });
   };
@@ -139,58 +143,3 @@ return (
 };
 
 export default Topics;
-
-
-/*
-return (
-  <main ref={myRef}>
-     <h2 className="topics__h2">Viewing all topics</h2>
-     {isLoading ? <div><Spinner animation="grow" /><p className="topics__loading">Fetching data...</p></div> : 
-     
-     <div>
-     {successMsg && <p className="topic__confirmation">Your topic has been added to the list.</p>}
-     {showDeletedMsg && <p className="comments__confirmation">Topic deleted.</p>}
-     {error && <p className="error">{error}</p>}
-     
-     <ul className={`topics__list ${topics.length > 5 ? '--flex' : ''}`}>
-        {topics.map(({slug, description, topic_id, number_of_articles}) => {
-          const cursorStyle = number_of_articles > 0 ? {'cursor' : 'pointer'} : {};
-              return (
-            <li 
-            key={topic_id} 
-            style={cursorStyle}
-            className={`topics__topic ${topics.length > 5 ? '--flex-item' : ''} ${number_of_articles === 0 ? '--empty-topic' : ''}`}
-            onClick={() => number_of_articles > 0 && navigate(`/articles?topic=${slug}`)}
-            >
-          
-            <p className='topic__title'>{`${slug}; ${number_of_articles === 1 ? `${number_of_articles} article` : `${number_of_articles} articles`}`}</p>
-            
-            <p className='topic__description'>"{description}"</p>
-            {number_of_articles === 0 && <p className="topic__empty-msg">No articles have been posted under this topic yet. Click the button to add an article, or to delete the topic.</p>}
-
-            {number_of_articles === 0 && <DropdownButton
-            id="dropdown-basic-button"
-            title="Select an option">
-              <Dropdown.Item onClick={() => navigate(`/articles/new?topic=${slug}`)}>Post an article</Dropdown.Item>
-              <Dropdown.Item onClick={handleDelete(slug)}>Delete topic</Dropdown.Item>
-              </DropdownButton>}
-            </li>)
-        })}
-    </ul>
-
-    <h3 className="topics__h3">Add a new topic</h3>
-    <form id="topic__form" onSubmit={handleSubmit}>
-        <label className="topic__form-label" htmlFor="topic__new-topic">Topic title</label>
-        <input id="topic__new-topic" type="text" onChange={e => setNewTopic(e.target.value)} value={newTopic}/>
-  
-        <label className="topic__form-label" htmlFor="topic__body">Topic description</label>
-        <textarea id="topic__body" type="text" onChange={e => setNewTopicBody(e.target.value)} value={newTopicBody}/>
-
-        <button style={{'backgroundColor' : newTopicLoading ? 'grey' : ''}} id="topic__submit">{newTopicLoading ? 'Adding topic...' : 'Create new topic'}</button>
-    </form>
-    </div>}
-  </main>
-);
-};
-
-*/
