d65540901ff469fccb18d12d401ed9943c380526
3782291211 <Laklouk98@hotmail.com>
2024-01-02 00:51:14 +0000

    Updating delete functionality for sample user

--- src/Components/DeleteAccount.jsx
+++ src/Components/DeleteAccount.jsx
@@ -35,16 +35,16 @@ const handleDelete = e => {
       backdrop={isDeleted ? 'static' : true}
     >
       <Modal.Header style={{display: 'flex'}} closeButton={!isDeleted}>
-      <Modal.Title id="contained-modal-title-vcenter">{isDeleted ? "Account deleted." : "⚠️ Warning: deleting your account is a permanent action and cannot be reversed"}</Modal.Title>
+      <Modal.Title id="contained-modal-title-vcenter">{isDeleted ? "Account deleted." : loggedInUser === "weegembump" ? "⚠️ Unable to delete sample user." : "⚠️ Warning: deleting your account is a permanent action and cannot be reversed"}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4 style={{fontSize: '24px'}}>{isDeleted ? "You're all done. Your account has been succesfully deleted." : "Are you sure you want to delete your account?"}</h4>
-        <p>{isDeleted ? "Feel free to continue browsing the website or make a new account!" : "This will also delete any articles and comments that you've posted."}</p>
+        <h4 style={{fontSize: '24px'}}>{isDeleted ? "You're all done. Your account has been succesfully deleted." : loggedInUser === "weegembump" ? "Action denied." : "Are you sure you want to delete your account?"}</h4>
+        <p>{isDeleted ? "Feel free to continue browsing the website or make a new account!" : loggedInUser === "weegembump" ? "The sample user is required for demonstration purposes and as such should not be deleted. The delete feature works for any self-created accounts." : "This will also delete any articles and comments that you've posted."}</p>
       </Modal.Body>
       <Modal.Footer>
         {!isDeleted && <Button onClick={onHide}>Close</Button>}
 
-        {!isDeleted && <Button style={{backgroundColor: '#EE2121', color: 'white', fontWeight: 'bold'}} onClick={handleDelete}>Delete account</Button>}
+        {!isDeleted && <Button style={{backgroundColor: '#EE2121', color: 'white', fontWeight: 'bold'}} onClick={handleDelete} disabled={loggedInUser === "weegembump"}>Delete account</Button>}
 
         {isDeleted && <Button onClick={() => {
           navigate('/');
